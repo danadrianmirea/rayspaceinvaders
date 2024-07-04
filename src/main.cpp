71d94f928c76753ef99eade6dff0e41cbfb0dfd9
Dan Adrian Mirea <damirea84@gmail.com>
2024-07-04 23:02:10 +0300

    enable borderless fullscreen by default

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,12 @@ std::string FormatWithLeadingZeroes(int number, int width)
 int windowWidth = 1920;
 int windowHeight = 1080;
 
-Color grey = Color{29, 29, 27, 255};
-Color yellow = Color{243, 216, 63, 255};
+Color grey = Color{ 29, 29, 27, 255 };
+Color yellow = Color{ 243, 216, 63, 255 };
 
 int offset = 50;
 
-void UpdateWindow(Game &game, float scale)
+void UpdateWindow(Game& game, float scale)
 {
     if (WindowShouldClose() || (IsKeyPressed(KEY_ESCAPE) && exitWindowRequested == false))
     {
@@ -111,7 +111,7 @@ int main()
     // MaximizeWindow();
     SetWindowPosition(50, 50);
     // ToggleFullscreen();
-    // ToggleBorderlessWindowed();
+    ToggleBorderlessWindowed();
 
     gameScreenWidth = gameScreenWidth + offset;
     gameScreenHeight = gameScreenHeight + 2 * offset;
@@ -126,7 +126,7 @@ int main()
     while (!exitWindow)
     {
         UpdateMusicStream(game.music);
-        
+
         scale = MIN((float)GetScreenWidth() / gameScreenWidth, (float)GetScreenHeight() / gameScreenHeight);
         UpdateWindow(game, scale);
 
@@ -135,25 +135,25 @@ int main()
         BeginTextureMode(target);
 
         ClearBackground(grey);
-        DrawRectangleRoundedLines({10, 10, 780, 780}, 0.18f, 20, 2, yellow);
+        DrawRectangleRoundedLines({ 10, 10, 780, 780 }, 0.18f, 20, 2, yellow);
 
-        DrawLineEx({25, 730}, {775, 730}, 3, yellow);
-        DrawTextEx(font, "LEVEL 01", {570, 740}, 34, 2, yellow);
+        DrawLineEx({ 25, 730 }, { 775, 730 }, 3, yellow);
+        DrawTextEx(font, "LEVEL 01", { 570, 740 }, 34, 2, yellow);
 
-        DrawTextEx(font, "SCORE", {50, 15}, 34, 2, yellow);
+        DrawTextEx(font, "SCORE", { 50, 15 }, 34, 2, yellow);
         std::string scoreText = FormatWithLeadingZeroes(game.score, 7);
-        DrawTextEx(font, scoreText.c_str(), {50, 40}, 34, 2, yellow);
+        DrawTextEx(font, scoreText.c_str(), { 50, 40 }, 34, 2, yellow);
 
-        DrawTextEx(font, "HIGH-SCORE", {570, 15}, 34, 2, yellow);
+        DrawTextEx(font, "HIGH-SCORE", { 570, 15 }, 34, 2, yellow);
         std::string highScoreText = FormatWithLeadingZeroes(game.highScore, 7);
-        DrawTextEx(font, highScoreText.c_str(), {570, 40}, 34, 2, yellow);
+        DrawTextEx(font, highScoreText.c_str(), { 570, 40 }, 34, 2, yellow);
 
         Texture2D spaceshipImage = LoadTexture("Graphics/spaceship.png");
 
         float x = 50.0f;
         for (int i = 0; i < game.lives; i++)
         {
-            DrawTextureV(spaceshipImage, {x, 745}, WHITE);
+            DrawTextureV(spaceshipImage, { x, 745 }, WHITE);
             x += 50;
         }
 
@@ -163,29 +163,33 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawTexturePro(target.texture, (Rectangle){0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height},
-                       (Rectangle){(GetScreenWidth() - ((float)gameScreenWidth * scale)) * 0.5f, (GetScreenHeight() - ((float)gameScreenHeight * scale)) * 0.5f,
-                                   (float)gameScreenWidth * scale, (float)gameScreenHeight * scale},
-                       (Vector2){0, 0}, 0.0f, WHITE);
+        DrawTexturePro(target.texture, (Rectangle) { 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height },
+            (Rectangle) {
+            (GetScreenWidth() - ((float)gameScreenWidth * scale)) * 0.5f, (GetScreenHeight() - ((float)gameScreenHeight * scale)) * 0.5f,
+                (float)gameScreenWidth* scale, (float)gameScreenHeight* scale
+        },
+            (Vector2) {
+            0, 0
+        }, 0.0f, WHITE);
 
         if (exitWindowRequested)
         {
-            DrawRectangleRounded({(float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120}, 0.76f, 20, BLACK);
+            DrawRectangleRounded({ (float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120 }, 0.76f, 20, BLACK);
             DrawText("Are you sure you want to exit? [Y/N]", GetScreenWidth() / 2 - 400, GetScreenHeight() / 2, 40, yellow);
         }
         else if (game.paused)
         {
-            DrawRectangleRounded({(float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120}, 0.76f, 20, BLACK);
+            DrawRectangleRounded({ (float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120 }, 0.76f, 20, BLACK);
             DrawText("Game paused, press P to continue", GetScreenWidth() / 2 - 400, GetScreenHeight() / 2, 40, yellow);
         }
         else if (game.lostWindowFocus)
         {
-            DrawRectangleRounded({(float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120}, 0.76f, 20, BLACK);
+            DrawRectangleRounded({ (float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120 }, 0.76f, 20, BLACK);
             DrawText("Game paused, focus window to continue", GetScreenWidth() / 2 - 400, GetScreenHeight() / 2, 40, yellow);
         }
         else if (game.gameOver)
         {
-            DrawRectangleRounded({(float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120}, 0.76f, 20, BLACK);
+            DrawRectangleRounded({ (float)(GetScreenWidth() / 2 - 500), (float)(GetScreenHeight() / 2 - 40), 1000, 120 }, 0.76f, 20, BLACK);
             DrawText("Game over, press SPACE to play again", GetScreenWidth() / 2 - 400, GetScreenHeight() / 2, 40, yellow);
         }
 

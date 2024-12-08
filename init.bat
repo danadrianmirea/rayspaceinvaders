491e5f4a2429a683c808eeeff7e3bfad13f35941
Dan Adrian Mirea <damirea84@gmail.com>
2024-12-08 20:04:08 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# rayspaceinvaders" >> README.md
-git init
-git add README.md
-git commit -m .
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/rayspaceinvaders.git
-git push -u origin main
